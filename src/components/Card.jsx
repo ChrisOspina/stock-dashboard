11e5d8b001321fb5866e33639e38a513af5ac9e5
ChrisOspina <ChrisOspina99@gmail.com>
2025-06-13 20:53:03 -0400

    Details component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Card = ({children}) =>{
-    return <div className="w-full h-full rounded-md relative p-8 border-2 bg-gray-300">{children}</div>
+    return (
+    <div className="w-full h-full rounded-md relative p-8 border-2 bg-white border-neutral-200">
+        {children}
+    </div>
+    );
 };
 
 export default Card
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import {mockSearchResults} from '../constants/mock';
 import {XIcon, SearchIcon} from '@heroicons/react/solid';
+import SearchResults from './SearchResults';
 
 const Search = ()=> {
     const [input, setInput] = useState("");
@@ -37,8 +38,12 @@ const Search = ()=> {
             <button onClick={updateBestMatches} className='h-8 w-8 bg-indigo-600 rounded-md flex justify-center m-1 p-2'>
                 <SearchIcon className='h-4 w-4 fill-gray-100'/>
             </button>
+
+            {input && bestMatches.length > 0 ? (
+                <SearchResults results={bestMatches}/> 
+        ): null}
         </div>
-    )
-}
+    );
+};
 
 export default Search
